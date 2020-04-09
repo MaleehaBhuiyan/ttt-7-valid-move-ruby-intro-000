7823476604f0eb907526c5a57d2d5a78d45d237f
Maleeha Bhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2020-04-09 20:27:37 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 # code your #valid_move? method here
-def valid_move?(board,index)
+def valid_move?(board,index.between)
   index.between?(0,8)
-  if !position_taken?(board,index) && board[index].between?(0,8)
+  if !position_taken?(board,index.between) && board[index].between?(0,8)
     true
   end
 end
